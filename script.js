beace94d660c57c274c68ab72426876383431df8
Aksh Bansal <63552235+Aksh-Bansal-dev@users.noreply.github.com>
2020-04-13 00:57:14 +0530

    bug fixed

--- script.js
+++ script.js
@@ -45,58 +45,77 @@ function add(index)
 	
 	if(arr[index]==null){
 		[o,x] = [x,o];
-		console.log(i)
+		console.log(index)
 		boxs[index].innerHTML = o;
 
 		arr[index] = o;
 		i++;
 		logic(o);
-		if(i==9){
-
-			result.innerHTML ="Draw";
-			logic(o);
-					
-			}
+		
 	}
+	// compAdd(o);
+
 }
 function logic(o){
+			if(i==9){
+				result.innerHTML ="Draw";
+				return "tie";
+			}
 			if(arr[0] == o && arr[1] ==o && arr[2] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[3] == o && arr[4] ==o && arr[5] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[6] == o && arr[7] ==o && arr[8] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[0] == o && arr[3] ==o && arr[6] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[1] == o && arr[4] ==o && arr[7] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[2] == o && arr[5] ==o && arr[8] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[0] == o && arr[4] ==o && arr[8] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
 			}
 			if(arr[2] == o && arr[4] ==o && arr[6] == o){
 				result.innerHTML = o +" is the winner!";
+				arrFill();
+				return o;
 					
-			}
-			
-
-
-
-			}
+			}			
+}
+function arrFill () {
+	for(let l = 0;l<9;l++){
+		
+		arr[l] = "";
+	}
+}
 
 function gameover(){
 	for(let l = 0;l<9;l++){
@@ -110,3 +129,85 @@ function gameover(){
 	 start();
 }
 
+// function compAdd (o) {
+
+// 	[o,x] = [x,o];
+
+// 	let temp = arr;
+// 	let ai = 1;
+// 	let bestScore = -1000;
+// 	let to = o;
+// 	let tx = x;
+
+// 	for(let j=0;j<9;j++){
+// 		if (arr[j] == null) {
+// 			temp[j] = o;
+// 			if(minimax(temp, 0)>=bestScore){
+// 				bestScore = minimax(temp, 0, false,to);
+// 				console.log(j + " this is j")
+// 				ai = j;
+// 			}
+// 		}
+// 	}
+	
+
+// 	if(arr[ai]==null){
+		
+// 		console.log(i)
+// 		boxs[ai].innerHTML = o;
+
+// 		arr[ai] = o;
+// 		i++;
+// 		logic(o);
+// 		if(i==9){
+
+// 			result.innerHTML ="Draw";
+// 			logic(o);
+					
+// 			}
+// 	}
+// }
+// let scores = {
+// 	X : 1,
+// 	O : -1,
+// 	tie : 0
+// }
+
+// function minimax(temp, depth, isMax, to){
+// 	let result = logic(o);
+	
+// 	if(result !== null){
+// 		return scores[result];
+// 	}
+// 	if(isMax){
+// 		let bestScores = -100000;
+// 		for(let l = 0; l<9;l++){
+// 			if (temp[l]==null) {
+// 				[to,tx] = [tx,to];
+// 				temp[l] = to;
+// 				let score = minimax(temp, depth+1,false, to);
+// 				temp[l] = null;
+// 				if(score>bestScores){
+// 					bestScores = score;
+// 				}
+// 			}
+// 		}
+// 		return bestScores;
+// 	}else{
+// 		let bestScores = 1000000;
+// 		for(let l = 0; l<9;l++){
+// 			if (temp[l]==null) {
+// 				[to,tx] = [tx,to];
+// 				temp[l] = tx;
+// 				let score = minimax(temp, depth+1,true, tx);
+// 				temp[l] = null;
+// 				if(score<bestScores){
+// 					bestScores = score;
+// 				}
+// 			}
+// 		}
+// 		return bestScores;
+
+// 	}
+
+// }
